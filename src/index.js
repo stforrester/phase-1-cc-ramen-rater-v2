057283d4fcd18f45cf65e0c493ca77856676d53d
Stuart Forrester <stforrester@gmail.com>
2024-11-07 13:47:53 -0600

    cc draft 2 - completes Deliverable 1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ const ramenURL = "http://localhost:3000/ramens";
 ////////////////
 // DOM Selectors
 ////////////////
-
+const ramenMenu = document.querySelector("#ramen-menu");
+const detailImg = document.querySelector(".detail-image");
+const name = document.querySelector(".name");
+const restaurant = document.querySelector(".restaurant");
+const rating = document.querySelector("#rating-display");
+const comment = document.querySelector("#comment-display");
 
 //////////////////
 // Fetch functions
@@ -27,9 +32,30 @@ const addSubmitListener = () => {
   // Add code
 }
 
-const displayRamens = () => {
+const displayRamens = (ramensArray) => {
   // Add code
+  //console.log(ramensArray);
+  ramensArray.forEach(renderRamenInMenu);
 };
+
+//////////////////////////////
+// Render Functions
+//////////////////////////////
+function renderRamenInMenu(ramenObject) {
+  //console.log(ramenObject);
+  const img = document.createElement("img");
+  //console.log(img);
+  img.src = ramenObject.image;
+  ramenMenu.append(img);
+}
+
+function renderRamenDetails(ramenObject) {
+  detailImg.src = ramenObject.image;
+  name.innerText = ramenObject.name;
+  restaurant.innerText = ramenObject.restaurant;
+  rating.innerText = ramenObject.rating;
+  comment.innerText = ramenObject.comment;
+}
 
 const main = () => {
   // Invoke displayRamens here
@@ -39,8 +65,18 @@ const main = () => {
 /////////////////////////////////////////
 // EventListener for DOM Load to run MAIN
 /////////////////////////////////////////
-//main()
-getAllRamen(ramenURL).then(console.log);
+main()
+
+
+/////////////////////////////
+//Initializer for development
+/////////////////////////////
+getAllRamen(ramenURL).then(ramenArray => {
+  //Test to see renderRamenDetails is working:
+  //renderRamenDetails(ramenArray[0]);
+  displayRamens(ramenArray);
+});
+
 
 // Export functions for testing
 export {
